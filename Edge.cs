7428bab82cbae44db0fc92315cdeac130a17af65
Alina <72575682+AlinaNistor@users.noreply.github.com>
2023-12-05 17:08:38 +0200

    Added Node and Edge classes

---
+++ Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntColony
+{
+    class Edge
+    {
+        public Node _nodeA;
+        public Node _nodeB;
+        public double _weight;
+
+        public Edge(Node nodeA, Node nodeB, double weight)
+        {
+            _nodeA = nodeA;
+            _nodeB = nodeB;
+            _weight = weight;
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntColony
+{
+    class Node
+    {
+        public int _x;
+        public int _y;
+        public int _resource;
+        public string _name;
+        public Node _toHome;
+        public Dictionary<string, Edge> _edges;
+        
+        public Node(int x, int y, int resource)
+        {
+            _x = x;
+            _y = y;
+            _resource = resource;
+            _name = $"{_x}_{_y}";
+        }
+
+        public void AddEdge(string nodeName, Edge edge)
+        {
+            _edges.Add(nodeName, edge);
+        }
+
+        public void AddToHome(Node node)
+        {
+            _toHome = node;
+        }
+    }
+}
