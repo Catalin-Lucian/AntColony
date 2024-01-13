d74557e2251a0631b92ec7f07c0222b285aa3c36
Mircea Mihai Adrian <62558071+mmirceaadrian@users.noreply.github.com>
2024-01-13 15:46:46 +0200

    added to home count to node class

--- Node.cs
+++ Node.cs
@@ -9,7 +9,9 @@ namespace AntColony
 
         public Dictionary<string, Edge> edges;
         public Position position;
+        
         public Node toHome;
+        public int nodesToHome;
 
         public Node(string name, int xPoint, int yPoint, int resource)
         {
@@ -17,6 +19,16 @@ namespace AntColony
             position = Position.FromPoints(xPoint, yPoint);
             this.resource = resource;
             edges = new Dictionary<string, Edge>();
+            nodesToHome = 0;
+        }
+
+        public Node(string name, int xPoint, int yPoint, int resource, int nodesToHome)
+        {
+            this.name = name;
+            position = Position.FromPoints(xPoint, yPoint);
+            this.resource = resource;
+            edges = new Dictionary<string, Edge>();
+            this.nodesToHome = nodesToHome;
         }
     }
 }
